c24cb3fbe48d8539292d80d8ad8a0328476202f4
Alex <alexeluro@gmail.com>
2020-08-17 01:17:45 +0100

    First commit

---
+++ app/src/main/java/com/inspiredcoda/customnotification/Constants.kt
@@ -0,0 +1,5 @@
+package com.inspiredcoda.customnotification
+
+const val CHANNEL_ID = "Notification_1"
+const val CHANNEL_NAME = "Custom Notification"
+const val NOTIFICATION_ID = 0
---
+++ app/src/main/java/com/inspiredcoda/customnotification/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.inspiredcoda.customnotification
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.renderscript.RenderScript
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var notification: Notification
+    private lateinit var manager: NotificationManagerCompat
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        createNotification(pendingIntent!!)
+
+        showNotification.setOnClickListener {
+            showNotification()
+        }
+
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =
+                NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                    lightColor = Color.RED
+                    enableLights(true)
+                }
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+    }
+
+    private fun createNotification(pendingIntent: PendingIntent){
+        notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Custom Notification")
+            .setContentText("A simple Notifiaction")
+            .setSmallIcon(R.drawable.ic_timeline)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        manager = NotificationManagerCompat.from(this)
+    }
+
+    private fun showNotification(){
+        manager.notify(NOTIFICATION_ID, notification)
+    }
+
+}
